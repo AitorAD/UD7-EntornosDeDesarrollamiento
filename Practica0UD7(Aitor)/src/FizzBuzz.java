ede3e2c6575e35fee3e7e2b17e6d995909202d1f
INFORMATICA\dam_ada <aduqueagullo@gmail.com>
2024-04-23 13:11:27 +0200

    Subida practica 0. FizzBuzz

---
+++ Practica0UD7(Aitor)/src/FizzBuzz.java
@@ -0,0 +1,12 @@
+public class FizzBuzz {
+    private static final int FIZZ = 3;
+    private static final int BUZZ = 5;
+
+    public static String juego(int input) {
+        String output = String.valueOf(input);
+        if (input % FIZZ == 0 && input % BUZZ == 0) output = "FizzBuzz";
+        else if (input % FIZZ == 0) output = "Fizz";
+        else if (input % BUZZ == 0) output = "Buzz";
+        return output;
+    }
+}
---
+++ Practica0UD7(Aitor)/src/FizzBuzzTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FizzBuzzTest {
+    /*
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1",
+            "2, 2",
+            "3, Fizz",
+            //"4", "5", "6","7", "8", "9", "10", "11", "12", "13", "14", "15"
+    })
+    public void testFizzBuzz(String input, String output) {
+        assertEquals(output, input);
+    }
+    */
+    @Test
+    public void test0() {
+        assertEquals("FizzBuzz", FizzBuzz.juego(0));
+    }
+
+    @Test
+    public void test1() {
+        assertEquals("1", FizzBuzz.juego(1));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("2", FizzBuzz.juego(2));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals("Fizz", FizzBuzz.juego(3));
+    }
+}
