8572965b02985370925ed1f812b147796648bb08
INFORMATICA\dam_ada <aduqueagullo@gmail.com>
2024-04-30 12:46:29 +0200

    Primer test Clase Zapping

--- Zapping/src/Zapping.java
+++ Zapping/src/Zapping.java
@@ -1,2 +1,22 @@
 public class Zapping {
+    private static final int MAX_CHANEL = 99;
+    private static final int MIN_CHANEL = 0;
+
+    public static int cambiaCanal(int canalOrigen, int canalDestino) {
+        int nClic = 0;
+        if (isValidChanel(canalOrigen) && isValidChanel(canalDestino)) {
+            if (canalOrigen != canalDestino) {
+                nClic++;
+            }
+        }
+        return nClic;
+    }
+
+    private static boolean isValidChanel(int canal) {
+        boolean isValidChanel = canal <= MAX_CHANEL && canal >= MIN_CHANEL;
+        if (isValidChanel) {
+            return true;
+        }
+        return false;
+    }
 }
--- Zapping/src/ZappingTest.java
+++ Zapping/src/ZappingTest.java
@@ -1,5 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ZappingTest {
-
+    @Test
+    public void test_Origen_0_Destino_0_nClicls_0() {
+        assertEquals(0, Zapping.cambiaCanal(0,0));
+    }
 }
