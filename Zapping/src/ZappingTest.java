44c09cb3ce793782a3ee4c2d3a8c9167949b12a6
INFORMATICA\dam_ada <aduqueagullo@gmail.com>
2024-04-30 12:47:42 +0200

    Segundo test Clase Zapping

--- Zapping/src/ZappingTest.java
+++ Zapping/src/ZappingTest.java
@@ -7,4 +7,9 @@ class ZappingTest {
     public void test_Origen_0_Destino_0_nClicls_0() {
         assertEquals(0, Zapping.cambiaCanal(0,0));
     }
+
+    @Test
+    public void test_Origen_1_Destino_2_nClicls_1() {
+        assertEquals(1, Zapping.cambiaCanal(1,2));
+    }
 }
